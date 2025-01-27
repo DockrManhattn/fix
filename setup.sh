ff82242425946adb16b64e6e690cf8ca05e5c7d1
DockrManhattn <38168619+DockrManhattn@users.noreply.github.com>
2025-01-26 22:30:29 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,4 +13,11 @@ cp ./fix "$HOME/.local/bin/"
 echo "Changing permissions to make fix executable"
 chmod +x "$HOME/.local/bin/fix"
 
+if ! command -v xdotool &> /dev/null; then
+  echo "xdotool not found, installing it now"
+  sudo apt update && sudo apt install -y xdotool
+else
+  echo "xdotool is already installed"
+fi
+
 echo "Setup complete!"
